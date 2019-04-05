7e9b5ffe57a7bbf2a96825e5a0481950453e883f
Hank Shen <swhbox@foxmail.com>
2019-04-05 23:12:48 +0800

    update

--- config/account.go
+++ config/account.go
@@ -4,13 +4,29 @@ import (
 	"github.com/webx-top/echo"
 )
 
+func NewAccount() *Account {
+	return &Account{}
+}
+
 type Account struct {
-	Debug      bool
-	AppID      string //即AppID
-	AppSecret  string //即AppKey
-	MerchantID string //商家ID
-	PublicKey  string //公钥
-	PrivateKey string //私钥
-	CertPath   string //证书路径
-	Options    echo.H //其它选项
+	Debug      bool   `json:"debug"`
+	AppID      string `json:"appID"`      //即AppID
+	AppSecret  string `json:"appSecret"`  //即AppKey
+	MerchantID string `json:"merchantID"` //商家ID
+	PublicKey  string `json:"publicKey"`  //公钥
+	PrivateKey string `json:"privateKey"` //私钥
+	CertPath   string `json:"certPath"`   //证书路径
+	Options    echo.H `json:"options"`    //其它选项
+}
+
+func (c *Account) FromStore(v echo.Store) *Account {
+	c.Debug = v.Bool(`debug`)
+	c.AppID = v.String(`appID`)
+	c.AppSecret = v.String(`appSecret`)
+	c.MerchantID = v.String(`merchantID`)
+	c.PublicKey = v.String(`publicKey`)
+	c.PrivateKey = v.String(`privateKey`)
+	c.CertPath = v.String(`certPath`)
+	c.Options = v.Store(`options`)
+	return c
 }
