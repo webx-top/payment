d47b7861575653dfabe1bb311a23ccd7ae878d15
Hank Shen <swhbox@foxmail.com>
2019-04-14 20:14:49 +0800

    update

--- payment.go
+++ payment.go
@@ -2,6 +2,7 @@ package payment
 
 import (
 	"sync"
+	"sort"
 
 	"github.com/webx-top/echo"
 	"github.com/webx-top/echo/param"
@@ -20,6 +21,15 @@ var (
 	mutex    = &sync.RWMutex{}
 	payments = map[config.Platform]func() Hook{}
 )
+
+func Platforms()[]string{
+	r:=[]string{}
+	for platform:=range payments{
+		r=append(r,string(platform))
+	}
+	sort.Strings(r) 
+	return r
+}
 
 func Register(name config.Platform, hook func() Hook) {
 	mutex.Lock()
