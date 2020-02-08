14befa2cdc167ed414bcfe90503d68b9926e1bf0
Hank Shen <swhbox@foxmail.com>
2020-02-08 21:59:48 +0800

    update

--- config/account.go
+++ config/account.go
@@ -8,15 +8,22 @@ func NewAccount() *Account {
 	return &Account{}
 }
 
+type Options struct {
+	IconClass string `json:"iconClass"`
+	IconImage string `json:"iconImage"`
+	Title     string `json:"title"`
+	Name      string `json:"name"`
+}
+
 type Account struct {
-	Debug      bool   `json:"debug"`
-	AppID      string `json:"appID"`      //即AppID
-	AppSecret  string `json:"appSecret"`  //即AppKey
-	MerchantID string `json:"merchantID"` //商家ID
-	PublicKey  string `json:"publicKey"`  //公钥
-	PrivateKey string `json:"privateKey"` //私钥
-	CertPath   string `json:"certPath"`   //证书路径
-	Options    echo.H `json:"options"`    //其它选项
+	Debug      bool    `json:"debug"`
+	AppID      string  `json:"appID"`      //即AppID
+	AppSecret  string  `json:"appSecret"`  //即AppKey
+	MerchantID string  `json:"merchantID"` //商家ID
+	PublicKey  string  `json:"publicKey"`  //公钥
+	PrivateKey string  `json:"privateKey"` //私钥
+	CertPath   string  `json:"certPath"`   //证书路径
+	Options    Options `json:"options"`    //其它选项
 }
 
 func (c *Account) FromStore(v echo.Store) *Account {
@@ -27,6 +34,10 @@ func (c *Account) FromStore(v echo.Store) *Account {
 	c.PublicKey = v.String(`publicKey`)
 	c.PrivateKey = v.String(`privateKey`)
 	c.CertPath = v.String(`certPath`)
-	c.Options = v.Store(`options`)
+	options := v.Store(`options`)
+	c.Options.IconClass = options.String(`iconClass`)
+	c.Options.IconImage = options.String(`iconImage`)
+	c.Options.Title = options.String(`title`)
+	c.Options.Name = options.String(`name`)
 	return c
 }
